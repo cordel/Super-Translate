a2c7bc70fb59251bacd738a1731fb849ac796e1c
suzdalnitskiy <senya@live.com>
2017-07-11 19:36:56 +0300

    mock testing repo

--- app/src/main/java/live/senya/supertranslate/data/source/Repository.kt
+++ app/src/main/java/live/senya/supertranslate/data/source/Repository.kt
@@ -5,17 +5,23 @@ import live.senya.supertranslate.data.Lang
 import live.senya.supertranslate.data.TextToTranslate
 import live.senya.supertranslate.data.Translation
 import live.senya.supertranslate.data.source.local.BaseLocalDataSource
-import live.senya.supertranslate.data.source.remote.yandex.YandexTranslateDataSource
+import live.senya.supertranslate.data.source.remote.RemoteDataSource
 
 class Repository(val localDataSource: BaseLocalDataSource,
-                 val remoteDataSource: YandexTranslateDataSource) {
+                 val remoteDataSource: RemoteDataSource) {
 
     fun getLangs(): Observable<List<Lang>> = localDataSource.getLangs()
+
+//    fun getTranslation(textToTranslate: TextToTranslate): Observable<Translation> {
+//        return localDataSource.getTranslation(textToTranslate)
+//                .switchIfEmpty(remoteDataSource.getTranslation(textToTranslate))
+//    }
 
     fun getTranslation(textToTranslate: TextToTranslate): Observable<Translation> {
         return localDataSource.getTranslation(textToTranslate)
                 .switchIfEmpty(remoteDataSource.getTranslation(textToTranslate))
     }
+
 
     fun putTranslationOnTopOfHistory(translation: Translation) = localDataSource.putTranslationOnTopOfHistory(translation)
 
---
+++ app/src/test/java/live/senya/supertranslate/RepositoryTest.kt
@@ -0,0 +1,62 @@
+package live.senya.supertranslate
+
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
+import live.senya.supertranslate.data.Lang
+import live.senya.supertranslate.data.TextToTranslate
+import live.senya.supertranslate.data.Translation
+import live.senya.supertranslate.data.source.Repository
+import live.senya.supertranslate.data.source.local.BaseLocalDataSource
+import live.senya.supertranslate.data.source.remote.RemoteDataSource
+import org.junit.Test
+import org.mockito.Mockito
+
+class RepositoryTest {
+
+    val lang = Lang("code", "name", "en")
+    val textToTranslate = TextToTranslate(lang, lang, "original text")
+    val translation = Translation(textToTranslate, "translated text")
+    val translation1 = Translation(textToTranslate, "translated text1")
+
+    @Test
+    fun repoQueriesRemoteDataSourceTest() {
+        val remoteDataSource = Mockito.mock(RemoteDataSource::class.java)
+        Mockito.`when`(remoteDataSource.getTranslation(textToTranslate))
+                .thenReturn(Observable.just(translation1))
+
+        val localDataSource = Mockito.mock(BaseLocalDataSource::class.java)
+        Mockito.`when`(localDataSource.getTranslation(textToTranslate))
+                .thenReturn(Observable.empty())
+
+        val repository = Repository(localDataSource, remoteDataSource)
+
+        val test = TestObserver<Translation>()
+
+        repository.getTranslation(textToTranslate).subscribe(test)
+
+        test.assertValue(translation1)
+        test.assertValueCount(1)
+        test.assertComplete()
+    }
+
+    @Test
+    fun repoQueriesOnlyLocalDataSourceTest() {
+        val remoteDataSource = Mockito.mock(RemoteDataSource::class.java)
+        Mockito.`when`(remoteDataSource.getTranslation(textToTranslate))
+                .thenReturn(Observable.just(translation1))
+
+        val localDataSource = Mockito.mock(BaseLocalDataSource::class.java)
+        Mockito.`when`(localDataSource.getTranslation(textToTranslate))
+                .thenReturn(Observable.just(translation))
+
+        val repository = Repository(localDataSource, remoteDataSource)
+
+        val test = TestObserver<Translation>()
+
+        repository.getTranslation(textToTranslate).subscribe(test)
+
+        test.assertValue(translation)
+        test.assertValueCount(1)
+        test.assertComplete()
+    }
+}
